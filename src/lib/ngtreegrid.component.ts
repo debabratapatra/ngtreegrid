6179af6c7ffe807c4d14ae3974abd5aa72c74100
debabrata <debabratapatra12@gmail.com>
2019-02-13 20:11:04 +0530

    Apply Column Renderer. Apply Group Renderer.

--- src/lib/ngtreegrid.component.ts
+++ src/lib/ngtreegrid.component.ts
@@ -9,8 +9,9 @@ export class NgtreegridComponent implements OnChanges {
 
   processed_data: any[] = [];
   expand_tracker: Object = {};
-  column_keys: string[] = [];
-  columns: Object = {};
+  total_columns: Number = 0;
+  group_by_keys: Object = {};
+  columns: any[] = [];
   default_config: Object = {
     add_class: 'plus',
     minus_class: 'minus'
@@ -35,7 +36,7 @@ export class NgtreegridComponent implements OnChanges {
     const tree_grid = this;
 
     // Make an array of group by key.
-    const reduced = data.reduce(function(m, d) {
+    this.group_by_keys = data.reduce(function(m, d) {
 
         // Check if group by key is already an array or not
         if (m[group_by] && !Array.isArray(m[group_by])) {
@@ -53,10 +54,10 @@ export class NgtreegridComponent implements OnChanges {
         return m;
     });
 
-    const group_by_keys = Object.keys(reduced);
+    const group_keys = Object.keys(this.group_by_keys);
 
-    group_by_keys.forEach(key => {
-      const items  = reduced[key];
+    group_keys.forEach(key => {
+      const items  = this.group_by_keys[key];
       tree_grid.expand_tracker[key] = 0;
 
       // Set Parent object.
@@ -75,20 +76,24 @@ export class NgtreegridComponent implements OnChanges {
     if (!this.config.group_by) {
       window.console.error('group_by field is mandatory!');
     }
-    this.columns = this.config.columns ? this.config.columns : {};
-    if (this.config.columns) {
-      this.column_keys = Object.keys(this.columns);
-      return;
+
+    this.columns = this.config.columns ? this.config.columns : [];
+
+    // If columns doesn't exist in user's object.
+    if (!this.config.columns) {
+      const column_keys = Object.keys(this.data[0]);
+
+      // Remove group by key.
+      column_keys.splice(column_keys.indexOf(this.config.group_by), 1);
+
+      column_keys.forEach(key => {
+        this.columns.push({'header': key});
+      });
+
+      this.total_columns = column_keys.length;
+    } else {
+      this.total_columns = this.columns.length;
     }
-    this.columns = {};
-    this.column_keys = Object.keys(this.data[0]);
-
-    // Remove group by key.
-    this.column_keys.splice(this.column_keys.indexOf(this.config.group_by), 1);
-
-    this.column_keys.forEach(key => {
-      this.columns[key] = key;
-    });
   }
 
   expandRow(id) {
