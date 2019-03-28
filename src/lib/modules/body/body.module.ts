c88ec3b38f675c2eee8fdcf04a42535087801f7c
debabrata <debabratapatra12@gmail.com>
2019-03-28 11:04:45 +0530

    Move Add row to a new component.

--- src/lib/modules/body/body.module.ts
+++ src/lib/modules/body/body.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TreeBodyComponent } from './components/tree-body/tree-body.component';
 import { CellModule } from '../cell/cell.module';
+import { AddRowComponent } from './components/add-row/add-row.component';
 
 @NgModule({
-  declarations: [TreeBodyComponent],
+  declarations: [TreeBodyComponent, AddRowComponent],
   imports: [
     CommonModule,
     CellModule,
   ],
-  exports: [TreeBodyComponent]
+  exports: [TreeBodyComponent, AddRowComponent]
 })
 export class BodyModule { }
