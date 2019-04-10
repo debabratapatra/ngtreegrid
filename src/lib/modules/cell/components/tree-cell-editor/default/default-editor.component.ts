3bf7ab9a3216a5b83988e0774b3382cc5ec5ffe3
debabrata <debabratapatra12@gmail.com>
2019-04-10 11:09:56 +0530

    Move components.

--- src/lib/modules/cell/components/tree-cell-editor/default/default-editor.component.ts
+++ src/lib/modules/cell/components/tree-cell-editor/default/default-editor.component.ts
@@ -1,13 +1,13 @@
 import { Output, EventEmitter } from '@angular/core';
 
 export class DefaultEditor implements Editor {
-    @Output() stopediting = new EventEmitter<any>();
+    @Output() canceledit = new EventEmitter<any>();
     @Output() editcomplete = new EventEmitter<any>();
     @Output() cellclick = new EventEmitter<any>();
 }
 
 export interface Editor {
-    stopediting: EventEmitter<any>;
+    canceledit: EventEmitter<any>;
     editcomplete: EventEmitter<any>;
     cellclick: EventEmitter<any>;
 }
